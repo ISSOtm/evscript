eadf05882079773e3e4a680b8be2020bbc269bf3
Eldred HABERT <me@eldred.fr>
2024-01-24 11:17:54 +0100

    Clean up LALRPOP build config a bit

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 extern crate lalrpop;
 
 fn main() {
-	lalrpop::process_root().unwrap();
+	lalrpop::Configuration::new()
+		.emit_rerun_directives(true)
+		.use_cargo_dir_conventions()
+		.process()
+		.unwrap();
 }
